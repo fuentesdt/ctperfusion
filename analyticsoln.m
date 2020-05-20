78a74ca17b784f3919b45de2886cb54349495dee
David Fuentes <fuentesdt@gmail.com>
2020-05-19 21:29:25 -0500

    WIP: ...

--- analyticsoln.m
+++ analyticsoln.m
@@ -33,7 +33,7 @@ for kkk  = 13:13
   % accumulate residual 
   currentrawdata = rawdce(kkk,:,:,:);
   %residual( ((kkk-1)*image1Dsize +1):kkk*image1Dsize )= double(currentrawdata(:)) - modelValues(:) ;
-  residual    = double(currentrawdata(:)) - double(modelValues(:)) ;
+  residual    =  double(modelValues(:)) - double(currentrawdata(:)) ;
   residual(workarray==0 ) = 0 ; 
   residual(workarray==aifLabelValue) = 0 ; 
   %residualtmp = double(currentrawdata(:)) - double(modelValues(:)) ;
@@ -47,7 +47,7 @@ for kkk  = 13:13
 
   % accumulate jacobian 
   derivValuestmp = (distanceImage.* alphaImage.^-2).* derivImage ;
-  derivValuestmp =  residual.*derivValuestmp(:);
+  derivValuestmp =  2. * residual.*derivValuestmp(:);
   jacbuildtmp = [1:length(indlabelval)]';
   jacbuildtmptwo =  jacbuildtmp(workarray~=0 & workarray~=aifLabelValue); 
   indlabelvaltmp =  indlabelval(workarray~=0 & workarray~=aifLabelValue); 
