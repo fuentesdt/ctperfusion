ccbce1d830025344126a7de72eb3c3e1b9966ff6
David Fuentes <fuentesdt@gmail.com>
2020-07-02 12:36:19 -0500

    WIP: reference values

---
+++ plotdata.m
@@ -0,0 +1,28 @@
+
+clear all
+close all
+
+
+for idata = 1:5
+   OutputBase        = ['Processed/',sprintf('%04d',idata),'/']
+   OutputCorrelation = [OutputBase , 'correlation']
+   globalid = readtable( [OutputBase,'globalid.csv'])
+   solution = readtable( [OutputBase,'solution.csv'])
+   meanglobalid = readtable( [OutputBase,'meanglobalid.csv'])
+   meansolution = readtable( [OutputBase,'meansolution.csv'])
+
+   % negative values are 0
+   filterIdx = find(meanglobalid.Mean ~= 19 );
+
+   set(gca,'FontSize',16)
+   handle = figure(1);
+   [myrho,mypval] = corr(solution.Mean(filterIdx ),meansolution.Mean(filterIdx ),'Type','Spearman')
+   plot(solution.Mean(filterIdx ), meansolution.Mean(filterIdx ),'x') 
+   hold
+   plot(solution.Mean(filterIdx ), myrho* solution.Mean(filterIdx ) ) 
+   xlabel('pixelwise avg speed [mm/s]')
+   ylabel('super pixel speed [mm/s]')
+   saveas(handle,OutputCorrelation ,'png')
+   figure(2); plot(globalid.Mean(filterIdx ), meanglobalid.Mean(filterIdx ),'x') 
+
+end
