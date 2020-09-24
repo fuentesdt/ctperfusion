5a2a22da0a1f5d5db8cd7f70d3e35bb5fc0f863d
David Fuentes <fuentesdt@gmail.com>
2020-09-23 20:35:55 -0500

    WIP:

--- poissonfft.m
+++ poissonfft.m
@@ -9,13 +9,13 @@ infomaskgradx = niftiinfo('Processed/0001/smoothgrad01.nii.gz');
 maskgradx = niftiread(infomaskgradx );
 
 infograd = niftiinfo('Processed/0001/testgrad.nii.gz');
-grad3d = 100.* single(niftiread(infograd));
+grad3d = .1* single(niftiread(infograd));
 
 [kX kY kZ ] = ndgrid([1:infomask.ImageSize(1)] ,[1:infomask.ImageSize(2)],[1:infomask.ImageSize(3)]);
 
-myeps = 1.e-6;
+myeps = 1.e-4;
 
-mydenom = ((kX-1).^2+(kY-1).^2+(kZ-1).^2 ).^(-1);
+mydenom = 4 * (  sin(pi*(kX-1)/infomask.ImageSize(1)).^2 + sin(pi*(kY-1)/infomask.ImageSize(2)).^2 + sin(pi*(kZ-1)/infomask.ImageSize(3)).^2 ).^(-1);
 mydenom (1,1,1) = 0;
 maskinverse = (vol3d+myeps).^(-1);
 
